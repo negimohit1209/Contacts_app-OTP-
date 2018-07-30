b47d3ecd38a00290d3a02c26691c8b1300855167
negimohit1209 <mohitsngh1997@gmail.com>
2018-07-30 20:50:33 +0530

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./DB/mogoose');
 var { Todo } = require('./model/Todo');
@@ -39,6 +40,23 @@ app.get('/todos', (req,res)=>{
     });    
 });
 
+/** 
+ * GET /todos/:id
+ * fetching individual todos
+*/
+app.get('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo)
+            return res.status(404).send()
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send()
+    });
+});
 app.listen(3000, ()=> console.log('started on port 3000'));
 
 module.exports = { app };
