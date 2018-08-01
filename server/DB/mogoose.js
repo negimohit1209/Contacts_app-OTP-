6c5c0ac48af0cdfbe9978d76e5c4dae48e7f3ec2
negimohit1209 <mohitsngh1997@gmail.com>
2018-08-01 18:14:21 +0530

    heroku setup take 2

--- server/DB/mogoose.js
+++ server/DB/mogoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;  // bcoz mongoose is not having promise by default
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'); // this is generally the url of the online database
-
+var databaseUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+mongoose.connect(databaseUrl); // this is generally the url of the online database
+console.log(databaseUrl);
 module.exports = {mongoose};
