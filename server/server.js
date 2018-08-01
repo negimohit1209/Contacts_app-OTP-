674e0f17a6eab0818f8069d4abc0274ff9dfda03
negimohit1209 <mohitsngh1997@gmail.com>
2018-08-01 21:00:44 +0530

    Add PATCH todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // var dotenv = require('dotenv').load(); // for loading the environment variables.
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./DB/mogoose');
@@ -78,6 +79,31 @@ app.delete('/todos/:id', (req,res) => {
     });
 });
 
+/**
+ * UPDATE /todos/:id
+ * updating the routes
+ */
+app.patch('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    var body =  _.pick(req.body, ['text', 'completed']);
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
+    if (_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch(e => {
+        res.status(400).send();
+    })
+});
 app.listen(port, ()=> console.log(`started on port ${port}`));
 
 module.exports = { app };
